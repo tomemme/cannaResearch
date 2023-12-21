fd56bd33daf65c6004b46cc440cd82d056260698
tomemme <thomas.emmerling@icloud.com>
2023-12-21 10:00:09 -0600

    academic section changes

--- resTest.py
+++ resTest.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from Bio import Entrez
 from urllib.parse import quote
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # Function to scrape titles from a given URL and search query
 def scrape_titles(url, search_query):
@@ -21,7 +23,7 @@ def scrape_titles(url, search_query):
         pubmed_ids = record["IdList"]
 
         # Define the maximum number of results you want (5 in this case)
-        max_results = 5
+        max_results = 10
 
         # Fetch and print the PMID and TI for each PubMed ID, up to the maximum
         for i, pubmed_id in enumerate(pubmed_ids):
@@ -50,7 +52,7 @@ def scrape_titles(url, search_query):
         response = requests.get(url)
 
         # Define the maximum number of results you want (5 in this case)
-        max_results = 5
+        max_results = 10
 
         # Parse the HTML response
         soup = BeautifulSoup(response.text, "html.parser")
@@ -72,7 +74,7 @@ def scrape_titles(url, search_query):
         response = requests.get(url)
 
         # Define the maximum number of results you want (adjust as needed)
-        max_results = 5
+        max_results = 10
 
         # Parse the HTML response
         soup = BeautifulSoup(response.text, "html.parser")
@@ -90,35 +92,83 @@ def scrape_titles(url, search_query):
 
     elif "academic.oup.com/journals" in url:
         # Send an HTTP GET request to the next URL
-        print("------Academic")
-        url = f"https://academic.oup.com/journals/advanced-search?q={search_query}"
 
-        response = requests.get(url) 
+        # print("------Academic")
+        # url = f"https://academic.oup.com/journals/advanced-search?q={search_query}"
 
+        # response = requests.get(url) 
+
+        # print (search_query)
+
+        # # Define the maximum number of results you want (adjust as needed)
+        # max_results = 5
+        # counter = 0
+
+        # # Parse the HTML response
+        # soup = BeautifulSoup(response.text, "html.parser")
+
+        # result_divs = soup.find_all("div", class_="sr-list al-article-box al-normal clearfix")
+
+        # # Extract and print the titles of the search results
+        # #results = soup.find_all("h4", class_="sri-title customLink")
         
+        # for result_div in result_divs:
+        #     print(result_div)
+        #     if counter >= max_results:
+        #         break
+        #     #navigate down nested classes to find title
+        #     h4_element = result.div.find("h4", class_="sri-title customLink")
+        #     if h4_element:
+        #         span_element = h4_element.find("span", class_="access-title")
+        #         if span_element:
+        #             title = span_element.get_text(strip=True)
+        #             if title:
+        #                 print(title)
+        #                 counter +=1
 
-        # Define the maximum number of results you want (adjust as needed)
-        max_results = 5
+        #version2.0
+        # search_query = "your_search_query_here"
+        # url = f"https://academic.oup.com/journals/advanced-search?q={search_query}"
 
-        # Parse the HTML response
-        soup = BeautifulSoup(response.text, "html.parser")
+        # # Configure the Selenium WebDriver (you'll need to have a compatible driver installed)
+        # driver = webdriver.Chrome()  # You can use a different driver if preferred
 
-        # Extract and print the titles of the search results
-        results = soup.find_all("h4", class_="sri-title customLink al-title at-sr-item-title-link")
-        counter = 0
-        for result in results:
-            if counter >= max_results:
-                break
-            title = result.get_text(strip=True)
-            if title:
-                print(title)
-                counter += 1
+        # try:
+        #     # Open the URL in the browser
+        #     driver.get(url)
+
+        #     # Wait for the page to load (you may need to adjust the wait time)
+        #     driver.implicitly_wait(10)  # Wait for up to 10 seconds for elements to appear
+
+        #     print("------Academic")
+        #     print(f"URL: {url}")
+
+        #     # Find all the titles using XPath (you may need to adjust the XPath)
+        #     titles = driver.find_elements(By.XPATH, "//div[@class='sr-list al-article-box al-normal clearfix']//span[@class='access-title']")
+
+        #     # Extract and print the titles
+        #     max_results = 5
+        #     counter = 0
+
+        #     for title in titles:
+        #         if counter >= max_results:
+        #             break
+        #         title_text = title.text.strip()
+        #         if title_text:
+        #             print(title_text)
+        #             counter += 1
+        # except Exception as e:
+        #     print(f"An error occurred: {e}")
+        # finally:
+        #     # Close the browser when done
+        #     driver.quit()
 
     #elif "www.jstor.org" in url:
         
 
 # Example usage:
 search_query = input("Enter the search query: ").strip()
+
 urls_to_search = [
     "https://pubmed.ncbi.nlm.nih.gov/",
     "https://scholar.google.com/",
